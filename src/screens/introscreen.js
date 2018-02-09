996f9d85fb1492c3bcfbb1fc9346183d73570194
SpectreLion <jose.tin96@hotmail.com>
2018-02-08 20:52:57 -0600

    EJEMPLO2

--- src/screens/introscreen.js
+++ src/screens/introscreen.js
@@ -8,16 +8,23 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHightlight
 } from 'react-native';
 
 
 
-export default class App extends Component<{}> {
+export default class App extends Component<Props> {
   render() {
     return (
       <View style ={styles.cotainer}>
-        <Image source={require('./../images/remindMe-logo.png')}/>
+        <Image
+          style ={styles.logo}
+          source={require('./../images/remindMe-logo.png')}/>
+        <TouchableHightlight style ={styles.secondaryButton}>
+          <Text>COMENZAR</Text>
+        </TouchableHightlight>
+        <Image source={require('./../images/avatars.png')} />
       </View>
     );
   }
@@ -26,6 +33,21 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container:{
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     background: 'rgb(237,24,72)'
+  },
+  logo:{
+    width:285,
+    height: 75
+  },
+  secondaryButton:{
+    height:55,
+    width; 325,
+    padding:16,
+    borderStyle: 'solid',
+    borderWidth:2,
+    borderColor: 'white',
+    borderRadius: 27
   }
-})
+});
